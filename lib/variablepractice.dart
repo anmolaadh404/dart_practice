f83912674a4746945d4ca9d5abe7383ccb19638a
anmolaadh404 <3280631@mbmc.edu.np>
2026-02-11 23:34:16 +0545

    Final and Cost DateTime.now()

--- lib/variablepractice.dart
+++ lib/variablepractice.dart
@@ -17,7 +17,7 @@
    int futureage = age + 5;
     print ('My name is $name, and I am $age years old. In 5 years I will be $futureage years old.'); */
 
-void main() {
+/*void main() {
   var somevalue1 = 10;
   final somevalue2 = 10;
   const somevalue3 = 10;
@@ -32,4 +32,23 @@ void main() {
   print(somevalue1);
   print(somevalue2);
   print(somevalue3);
+} */
+
+/* void main() {
+  int age = 22;
+  age = 23; // mutable variable can be reassigned
+  print(age);
+}   */
+/* void main() {
+  final int age = 22;
+  // age = 23; // This line would cause an error because final variables cannot be reassigned
+  print(age);
+} */
+
+void main() {
+  final somevalue = DateTime.now();
+  const someconstvalue =
+      DateTime.now(); // This line would cause an error because const variables must be compile-time constants
+  print(somevalue);
+  print(someconstvalue);
 }
