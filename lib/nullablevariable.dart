c4f6e64591f37211823a330ec420af70b7ba7b80
anmolaadh404 <3280631@mbmc.edu.np>
2026-02-12 23:20:47 +0545

    Nullable variable

--- lib/nullablevariable.dart
+++ lib/nullablevariable.dart
@@ -4,6 +4,22 @@ void main() {
 
   String? name; // nullable variable, can hold a string or null
 
+  int number = 10;
+  print(number);
+
   name = 'Anmol';
   print(name);
+
+  int? age;
+  age = 22;
+  print(age);
+
+  String? someValue;
+  print(someValue?.length); // Output: null
+
+  someValue = 'Hello, Dart!';
+  print(someValue.length); // Output: Hello, Dart!
+
+  someValue = '2025';
+  print(someValue);
 }
