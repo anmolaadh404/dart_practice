5074fec73313ec02985d0fa2a872533aa4de2319
anmolaadh404 <3280631@mbmc.edu.np>
2026-02-15 22:56:55 +0545

    Relational Operators and assignment operators

--- lib/controlflow.dart
+++ lib/controlflow.dart
@@ -1,11 +1,14 @@
+bool isadult = false;
+// This is a simple Dart program that demonstrates the use of control flow statements (if-else) to determine if a person is an adult or a minor based on their age. The variable 'isadult' is set to false, and the program checks the age variable to print the appropriate message.
+// (==/!) = Relational operators.
+// && = Logical AND operator.
+// (=) = Assignment operator.
 void main() {
-  int age = 20;
-  if (age >= 18) {
-    //IF else statement
-    print('You are an adult');
-  }
+  int age = 17;
 
-  if (age >= 21) {
+  if (isadult) {
+    print('You are an adult 21');
+  } else if (age >= 18) {
     print('You are an adult');
   } else {
     print('You are a Minor');
