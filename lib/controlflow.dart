54db8257b9fb293c55a7b1abc662285fbfd17879
anmolaadh404 <3280631@mbmc.edu.np>
2026-02-15 22:41:22 +0545

    If else

--- lib/controlflow.dart
+++ lib/controlflow.dart
@@ -1,7 +1,11 @@
 void main() {
-  int age = 13;
+  int age = 20;
   if (age >= 18) {
     //IF else statement
+    print('You are an adult');
+  }
+
+  if (age >= 21) {
     print('You are an adult');
   } else {
     print('You are a Minor');
